beee326e4e7b85c7389e3700309653d35566cd4a
peter <lih@lafayette.edu>
2022-04-23 02:25:49 -0400

    bug fixes for board

--- src/Board.hs
+++ src/Board.hs
@@ -4,21 +4,31 @@ import Data.Char (chr)
 
 
 data Row
-  = Row {len :: Int, alive :: Bool, cells :: [Bool]}
+  = Row {len :: Int, cells :: [Bool]}
   deriving Eq
   
 instance Show Row where
-  show (Row _ _ c) = map (\x -> chr (48 + if x then 1 else 0)) c
+  show (Row _ c) = map (\x -> chr (48 + if x then 1 else 0)) c
+
+stringToList :: String -> [Bool]
+stringToList "" = []
+stringToList (x:xs) = (x == '1') : stringToList xs
+
+instance Read Row where
+  readsPrec _ s = let r = stringToList s
+    in [(Row (length r) (stringToList s), "")]
 
 makeRow :: [Bool] -> Row
-makeRow list = Row (length list) (or list) list
-
-emptyRow :: Row
-emptyRow = Row 0 False (repeat False)
+makeRow list = Row (length list) list
 
 -- num rows, num columns, list of rows
-data Board = Board Int Int [Row]
+data Board = Board {rowNum :: Int, columnNum ::Int, boardData :: [Row]}
   deriving (Eq, Show)
+
+instance Read Board where
+  readsPrec _ s =
+    let b = map read (lines s) :: [Row]
+     in [(Board (length b) (len (head b)) b, "")]
 
 -- zip everything and lazily evaluate them
 
--- src/Simulation.hs
+++ src/Simulation.hs
@@ -1,9 +1,13 @@
 module Simulation where
 
 import Board
+import Data.Char (chr)
 
 data Triple = Triple Bool Bool Bool
-  deriving (Show, Eq)
+  deriving (Eq)
+
+instance Show Triple where
+  show (Triple a b c) = chr (48 + fromEnum a) : chr (48 + fromEnum b) : [chr (48 + fromEnum c)]
 
 emptyTriple :: Triple
 emptyTriple = Triple False False False
@@ -12,52 +16,60 @@ tupleToTriple :: (Bool, Bool, Bool) -> Triple
 tupleToTriple (a, b, c) = Triple a b c
 
 data Cell = Cell Triple Triple Triple
-  deriving (Show, Eq)
+  deriving (Eq)
+
+instance Show Cell where
+  show (Cell a b c) = '(' : show a ++ show b ++ show c ++ [')']
 
 tupleToCell :: (Triple, Triple, Triple) -> Cell
 tupleToCell (a, b, c) = Cell a b c
 
+zip3ListRecursive :: a -> [a] -> [(a, a, a)]
+zip3ListRecursive zero (x : xs : xss : xsss) = (x, xs, xss) : zip3ListRecursive zero (xs : xss : xsss)
+zip3ListRecursive zero [x, xs] = [(x, xs, zero)]
+zip3ListRecursive _ _ = []
+
 zip3List :: a -> [a] -> [(a, a, a)]
-zip3List zero (x : xs : xss : xsss) = (x, xs, xss) : zip3List zero (xs : xss : xsss)
-zip3List zero (x : xs : _) = [(x, xs, zero)]
+zip3List zero list@(x : xs : _) = (zero, x, xs) : zip3ListRecursive zero list
 zip3List _ _ = []
 
-zipRow :: [Triple] -> [Cell]
-zipRow list = map tupleToCell (zip3List emptyTriple list)
+triplesToCells :: [Triple] -> [Cell]
+triplesToCells list = map tupleToCell (zip3List emptyTriple list)
 
 reduceCell :: Cell -> Bool
 reduceCell (Cell (Triple a b c) (Triple d e f) (Triple g h i)) =
-  let s = sum (map (\x -> if x then 1 else 0) [a, b, c, d, e, f, g, h, i]) :: Int
+  let s = sum (map fromEnum [a, b, c, d, f, g, h, i]) :: Int
    in (e && (s == 2 || s == 3)) || (not e && s == 3)
 
 appendCell :: Cell -> ([Bool], Int) -> ([Bool], Int)
 appendCell cell (lst, num) =
   let c = reduceCell cell
-   in if num > 0
-        then (c : lst, num + 1)
-        else if c then ([c], 1) else (lst, 0)
+   in (c : lst, num + 1)
 
 reduceRow :: [Cell] -> Row
 reduceRow row =
   let (list, num) = foldr appendCell ([], 0) row
-   in Row num (num > 0) list
+   in Row num list
 
 zipRows :: (Row, Row, Row) -> [Triple]
-zipRows (Row _ _ r1, Row _ _ r2, Row _ _ r3) = map tupleToTriple (zip3 r1 r2 r3)
+zipRows (Row _ r1, Row _ r2, Row _ r3) = map tupleToTriple (zip3 r1 r2 r3)
 
-computeRows :: [Row] -> [Row]
-computeRows rows =
-  let newRows =
-        map
-          zipRows
-          ( zip3
-              (emptyRow : emptyRow : rows)
-              (emptyRow : rows ++ [emptyRow])
-              (drop 1 rows ++ [emptyRow, emptyRow])
-          )
-   in map (reduceRow . zipRow) newRows
+rowsToCells :: [Row] -> Int -> [[Cell]]
+rowsToCells rows r1 =
+  let empty = Row r1 (replicate r1 False)
+   in map
+        (triplesToCells . zipRows)
+        ( zip3
+            (empty : rows)
+            rows
+            (drop 1 rows ++ [empty])
+        )
+
+computeRows :: [Row] -> Int -> [Row]
+computeRows [] _ = []
+computeRows rows l = map reduceRow (rowsToCells rows l)
 
 next :: Board -> Board
-next (Board _ _ rows) =
-  let resultingRows = computeRows rows
+next (Board _ columnCount rows) =
+  let resultingRows = computeRows rows columnCount
    in Board (length resultingRows) (foldr (max . len) 0 resultingRows) resultingRows
